966a06318f61e89c3390301aa4a055625ba16f04
Aug1998 <augustopruvost@gmail.com>
2026-05-26 20:24:24 -0300

    create theme file

---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    textBlack: "#18181b",
+    textGrey: "#71717a",
+    primaryLight: "#f8fafc",
+    primary: "#4f46e5",
+    primaryDark: "#1d15ad",
+    white: "#fff",
+    grey: "#e4e4e7",
+    black: "#000",
+    slate: "#334155",
+  },
+  fonts: {
+    main: 'Arial, sans-serif',
+  },
+};
+
+export default theme;
