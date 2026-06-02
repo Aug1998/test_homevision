340c1d7feb7267533d91f70ec12babe35838f771
Aug1998 <augustopruvost@gmail.com>
2026-06-02 00:16:05 -0300

    style favorites page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,11 +1,9 @@
 import styled from '@emotion/styled'
-import Filters from '../components/Filters'
 import ScrollingArea from '../components/ScrollingArea'
 
 export default function Favorites() {
   return (
     <Main>
-      <Filters />
       <ScrollingArea />
     </Main>
   )
@@ -14,15 +12,5 @@ export default function Favorites() {
 const Main = styled.div`
   width: 100%;
   max-width: 82rem;
-  display: grid;
-  grid-template-columns: 1fr 3fr;
-  grid-template-areas: "filters content";
-  gap: 1rem;
   margin-top: 1rem;
-  @media (max-width: 768px) {
-    grid-template-columns: 1fr;
-    grid-template-areas: 
-      "filters"
-      "content";
-  }
 `
